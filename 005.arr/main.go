6dc72eaf2bdfef7e1385a9e35e95abeb4d1dfa85
zhangYanWen <zhang_yanwen003@163.com>
2023-05-23 00:20:16 +0800

    数组的定义

--- 005.arr/main.go
+++ 005.arr/main.go
@@ -12,4 +12,24 @@ func main() {
 	fmt.Println(ages2)
 	ages3 := [2]int{31, 23}
 	fmt.Println(ages3)
+	ages4 := [5]int{}
+	fmt.Println(ages4)
+	ages4[0] = 1
+	ages4[1] = 11
+	ages4[2] = 1111
+	ages4[3] = 111
+	ages4[4] = 11111
+	fmt.Println(ages4)
+	/*
+	   遍历数组里面的元素
+	*/
+	for i := 0; i < len(ages4); i++ {
+		fmt.Println(ages4[i])
+	}
+	for j := range ages {
+		fmt.Println(ages[j])
+	}
+	for h, value := range ages2 {
+		fmt.Printf("%d:%d\n", h, value)
+	}
 }
